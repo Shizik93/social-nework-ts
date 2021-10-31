2819a3103e618c846c0465988f9f589e671ce3cd
AleksandrKayuda <akayuda999@gmail.com>
2021-10-31 18:43:48 +0300

    delete.request,post.request

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -3,8 +3,14 @@ import React from "react";
 import m from "./users.module.css";
 import img from "./userImg.png";
 import { NavLink } from "react-router-dom";
+import axios from "axios";
 
+type followType={
 
+    resultCode: number
+    messages:[],
+    data: object
+}
 type usersPropsType = {
     users: Array<UserType>,
     Follow: (id: number) => void,
@@ -45,8 +51,27 @@ export function Users(props: usersPropsType) {
                     <div>{f.name}</div>
                     <div>{f.status}</div>
                     {f.followed ?
-                        <button onClick={() => props.UnFollow(f.id)}>Unfollow</button> :
-                        <button onClick={() => props.Follow(f.id)}>Follow</button>}
+                        <button onClick={() => {
+                            axios.delete<followType>(`https://social-network.samuraijs.com/api/1.0/follow/${f.id}`,
+                                {withCredentials: true,
+                                    headers:{
+                                    'API-KEY':'a9f5b347-68da-4301-888d-541b1ac92546'
+                                }})
+                                .then(responce => {
+                                if(responce.data.resultCode===0){props.UnFollow(f.id)}
+
+                            })
+                            }}>Unfollow</button> :
+                        <button onClick={() => {  axios.post<followType>(`https://social-network.samuraijs.com/api/1.0/follow/${f.id}`,
+                            {},
+                            {withCredentials: true,
+                                headers:{
+                                'API-KEY':'a9f5b347-68da-4301-888d-541b1ac92546'
+                            }})
+                            .then(responce => {
+                            if(responce.data.resultCode===0){props.Follow(f.id)}
+
+                        })}}>Follow</button>}
 
 
                 </div>)
