b47cb5f30f94b3ecedffa978e3e6339f7231a9dc
Shizik93 <akayuda93@gmail.com>
2022-11-27 22:22:43 +0300

    Updated paths

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import {Nav} from "./components/Navbar/Nav";
 import Music from "./components/Music/Music";
 import News from "./components/News/News";
 import Settings from "./components/Settings/Settings";
-import {Route} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import {DialogsContainer} from "./components/Dialogs/DialogsContainer";
 import {UsersContainer} from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
@@ -34,13 +34,16 @@ function App() {
           <HeaderContainer/>
           <Nav/>
           <div className='app-wrapper-content'>
-            <Route path='/users' render={() => <UsersContainer/>}/>
-            <Route path='/dialogs' render={() => <DialogsContainer/>}/>
-            <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
-            <Route path='/music' render={() => <Music/>}/>
-            <Route path='/news' render={() => <News/>}/>
-            <Route path='/settings' render={() => <Settings/>}/>
-            <Route path='/login' render={() => <Login/>}/>
+              <Routes>
+                  <Route path='/users' element={<UsersContainer/>}/>
+                  <Route path='/dialogs' element={<DialogsContainer/>}/>
+                  <Route path='/profile/:userId?' element={<ProfileContainer/>}/>
+                  <Route path='/music' element={<Music/>}/>
+                  <Route path='/news' element={<News/>}/>
+                  <Route path='/settings' element={<Settings/>}/>
+                  <Route path='/login' element={<Login/>}/>
+              </Routes>
+
 
 
           </div>
